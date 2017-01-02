4e4173736c5b386520f23bb990d03cd8d8f5689f
Cody <cliske94@outlook.com>
2017-01-02 12:55:10 -0500

    Update stack.c
    C Stack Implementation

--- stack.c
+++ stack.c
@@ -1,2 +1,52 @@
-#include <stdio.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <assert.h>
+ #include <string.h>
+ #include "stack.h"
+ #define true 1
+ #define false 0
 
+void structNew(stack *s) {
+	s->loglength=0;
+	s->alloclength=4;
+	s->elems=malloc(4*sizeof(char *));
+	}
+	
+void structRem(stack *s) {
+	if (s->elems==NULL) {
+    printf("Stack is empty!\n");
+    return;
+  }
+	free(s->elems);
+	s->elems=NULL;
+	}
+	
+bool isEmpty(stack *s) {
+	if (s->elems==NULL) return true;
+	else return false;
+	}
+
+void structPush(stack *s, const char *x) {
+	if (s->loglength==s->alloclength) {
+		printf("Resizing \n");
+		s->alloclength*=2;
+		s=realloc(s->elems,(s->alloclength)*sizeof(char *));
+		assert(s!=NULL);
+		}
+	strcpy((s->elems)+(s->loglength*sizeof(char *)), x);
+	s->loglength++;
+	}
+	
+char *structPop(stack *s) {
+	if (isEmpty(s)==true) {
+		printf("Stack Overflow!");
+		return NULL;
+		}
+	else if (s->loglength==1) {
+		s->elems=NULL;
+		s->loglength--;
+		return NULL;
+		}
+	s->loglength--;
+	return s->elems+(s->loglength+1);
+	}
